9cd1bc48a1e142094a5f11e0a23f862b7fd5d173
HARBHeEY <abiodunadeiye1@gmail.com>
2026-04-04 01:59:44 +0100

    Add FAQ component with toggle functionality

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
+import Faq from "../components/home/Faq";
 import { HeroSection } from "../components/home/HeroSection";
 
 const Home = () => {
   return (
     <main>
       <HeroSection />
+      <Faq />
     </main>
   );
 };
